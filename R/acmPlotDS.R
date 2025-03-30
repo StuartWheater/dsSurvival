c89d52191cce08d2d7d30856134612b2e5206df3
ESCRI11 <xavier.escriba.montagut@gmail.com>
2025-03-30 12:11:57 +0200

    fix: exists

--- R/acmPlotDS.R
+++ R/acmPlotDS.R
@@ -8,14 +8,16 @@
 #' @author Xavier Escribà Montagut, 2025
 #' @export
 acmPlotDS <- function(pred_obj = NULL) {
-  pred_data <- eval(parse(text=pred_obj), envir = parent.frame())
-  if (!exists(pred_data)) {
-    stop("The specified prediction object does not exist", call.=FALSE)
+
+  if (!exists(pred_obj, envir = parent.frame())) {
+    stop("The specified pred_obj does not exist", call.=FALSE)
   }
 
   if(privacy_level != 'permissive' || privacy_level != 'avocado'){
     stop("This function requires the server to be in permissive mode", call.=FALSE)
   }
+
+  pred_data <- eval(parse(text=pred_obj), envir = parent.frame())
 
   if (!inherits(pred_data, "Predict")) {
     stop("The provided object is not of class 'Predict'", call.=FALSE)
