ad6edb4e9ca13d560ea1868c1dedbd0995dc697a
Stuart Wheater <stuart.wheater@arjuna.com>
2025-04-30 13:43:36 +0100

    initial arg tests

---
+++ tests/testthat/test-arg-finegrayDS.R
@@ -0,0 +1,50 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2025 XXXX
+# Copyright (c) 2024 Arjuna Technologies, Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+context("finegrayDS::arg::setup")
+
+set.standard.disclosure.settings()
+
+#
+# Tests
+#
+
+context("finegrayDS::arg::no parameters")
+test_that("no parameters finegrayDS", {
+    expect_error(finegrayDS(), "Both 'formula' and 'data' parameters must be provided", fixed = TRUE)
+})
+
+context("finegrayDS::arg::null parameters")
+test_that("null parameters finegrayDS", {
+    expect_error(finegrayDS(NULL), "Both 'formula' and 'data' parameters must be provided", fixed = TRUE)
+})
+
+context("finegrayDS::arg formula is null, data no value")
+test_that("formula is null, data no value", {
+    expect_error(finegrayDS(formula = "a ~ b"), "Both 'formula' and 'data' parameters must be provided", fixed = TRUE)
+})
+
+context("finegrayDS::arg formula no value, data is null")
+test_that("formula no value, data is null", {
+    expect_error(finegrayDS(data = NULL), "Both 'formula' and 'data' parameters must be provided", fixed = TRUE)
+})
+
+#
+# Done
+#
+
+context("finegrayDS::arg::shutdown")
+
+context("finegrayDS::arg::done")
---
+++ tests/testthat/test-arg-predictDS.R
@@ -0,0 +1,54 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2025 XXXX
+# Copyright (c) 2024 Arjuna Technologies, Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+context("predictDS::arg::setup")
+
+set.standard.disclosure.settings()
+
+#
+# Tests
+#
+
+context("predictDS::arg::no parameters")
+test_that("no parameters predictDS", {
+    expect_error(predictDS(), "invalid first argument", fixed = TRUE)
+})
+
+context("predictDS::arg::null parameters")
+test_that("null parameters predictDS", {
+    expect_error(predictDS(NULL), "invalid first argument", fixed = TRUE)
+})
+
+context("predictDS::arg::null parameter value")
+test_that("null parameter value predictDS", {
+    object <- NULL
+
+    expect_error(predictDS("object"), "The specified model object does not exist", fixed = TRUE)
+})
+
+context("predictDS::arg::number parameter value")
+test_that("number parameter value predictDS", {
+    object <- 1.0
+
+    expect_error(predictDS("object"), "The specified model object does not exist", fixed = TRUE)
+})
+
+#
+# Done
+#
+
+context("predictDS::arg::shutdown")
+
+context("predictDS::arg::done")
