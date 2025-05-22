1943704ea9365f3b9d8ace76017a997b7735b43f
Stuart Wheater <stuart.wheater@arjuna.com>
2025-05-22 16:54:41 +0100

    Extended tests

--- tests/testthat/test-smk-datadistDS.R
+++ tests/testthat/test-smk-datadistDS.R
@@ -21,10 +21,38 @@ set.standard.disclosure.settings()
 # Tests
 #
 
-context("datadistDS::smk::data not data.frame")
-test_that("data not data.frame", {
+context("datadistDS::smk::simple min example")
+test_that("simple min example", {
     data_arg      <- "data_obj"
     adjust_to_obj <- list(a = 'min')
+    data_obj      <- data.frame(a = c(0, 1, 2, 3, 4, 5), b = c(4, 5, 6, 7, 8, 9))
+
+    res <- datadistDS(data = data_arg, adjust_to_obj)
+    expect_equal(class(res), "datadist")
+    expect_length(res, 2)
+    expect_equal(class(res[1]), "list")
+    expect_equal(class(res[2]), "list")
+})
+
+
+context("datadistDS::smk::simple max example")
+test_that("simple max example", {
+    data_arg      <- "data_obj"
+    adjust_to_obj <- list(a = 'max')
+    data_obj      <- data.frame(a = c(0, 1, 2, 3, 4, 5), b = c(4, 5, 6, 7, 8, 9))
+
+    res <- datadistDS(data = data_arg, adjust_to_obj)
+    expect_equal(class(res), "datadist")
+    expect_length(res, 2)
+    expect_equal(class(res[1]), "list")
+    expect_equal(class(res[2]), "list")
+})
+
+
+context("datadistDS::smk::simple mean example")
+test_that("simple mean example", {
+    data_arg      <- "data_obj"
+    adjust_to_obj <- list(a = 'mean')
     data_obj      <- data.frame(a = c(0, 1, 2, 3, 4, 5), b = c(4, 5, 6, 7, 8, 9))
 
     res <- datadistDS(data = data_arg, adjust_to_obj)
